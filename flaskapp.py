ccafc5aaa72015ba695dee68ade89623429c736b
orgilemu <munkh-orgil@Mac.drake.edu>
2026-03-26 15:26:09 -0500

    added artistquery

--- flaskapp.py
+++ flaskapp.py
@@ -77,6 +77,21 @@ def viewdb():
 
 # TODO: Section 2 — add your /artistquery/<artist_id> route here
 
+@app.route("/artistquery/<artist_id>")
+def artist_query(artist_id):
+
+    rows = execute_query("""
+        SELECT ArtistId, Artist.Name, Track.Name, UnitPrice
+        FROM Artist
+        JOIN Album USING (ArtistID)
+        JOIN Track USING (AlbumID)
+        WHERE ArtistId = %s
+    """, 
+        (artist_id,)
+    )
+    
+    return display_html(rows)
+
 # TODO: Section 3 — add your /pricequerytextbox GET and POST routes here
 
 # TODO: Section 3 — add your /timequerytextbox GET and POST routes here
